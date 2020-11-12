15de969b6ffe881e663a0baaf7f80f838b9df331
caitlinbou <caitlin.bouroncle@gmail.com>
2020-11-12 09:35:15 -0800

    commit to pull

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 // import Badge from '@material-ui/core/Badge';
