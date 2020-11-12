538cc4558b9550e57ef8e275040c6a07a605c237
caitlinbou <caitlin.bouroncle@gmail.com>
2020-11-12 11:46:50 -0800

    map over array to put recent search on home page

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import '../Home/Home.css'
-import Search2 from "../../Search/Search2"
+import React, { Component } from "react";
+import "../Home/Home.css";
+import Search2 from "../../Search/Search2";
+import RecentCard from "../../Recent/Recent";
+import plants from "./plantArray.json";
 
-export default function Home() {
+class Home extends Component {
+  
+    state = {
+    plants
+  };
+
+  removePLant = id => {
+      const plants = this.state.plants.filter(plant => plant.id !== id);
+      this.setState({plants})
+  }
+
+  render() {
     return (
-        <div>
-            <Search2/>
-            <h2>Home</h2>
-        </div>
-    )
+      <div>
+        <Search2 />
+        <h2>Home</h2>
+        {this.state.plants.map(plant => (
+          <RecentCard
+            id={plant.id}
+            name={plant.name}
+            info={plant.info}
+            image={plant.image}
+          />
+        ))}
+      </div>
+    );
+  }
 }
+
+export default Home;
