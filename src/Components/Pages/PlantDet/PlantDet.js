25c0ff41f6cd8ee6d6759d840267548c8e814d34
Maria Waslick <mariawaslick@gmail.com>
2020-11-13 14:26:40 -0500

    got basic plant details page up

--- src/Components/Pages/PlantDet/PlantDet.js
+++ src/Components/Pages/PlantDet/PlantDet.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import '../PlantDet/PlantDet.css'
+import React, { useEffect, useState } from 'react'
+import API from '../../../utils/API';
+import { makeStyles } from '@material-ui/core/styles';
+import PlantSearchCard from '../../PlantSearchCard/PlantSearchCard';
+
+const useStyles = makeStyles((theme) => ({
+
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap'
+    },
+    button: {
+      margin: theme.spacing(1),
+      backgroundColor: "green"
+    }
+  }));
 
 export default function PlantDet() {
+    const [plantDetails, setPlantDetails] = useState([])
+    const [comments, setComments] = useState([])
+    useEffect(() => {
+        API.getPlantID("5faed93cd337fd9fc042df4c")
+        .then (result => {
+            console.log(result.data)
+            setPlantDetails(result.data.dbPlant)
+            setComments(result.data.dbComment)
+        })
+    })
+
+
     return (
-        <div>
-            <h2>PlantDet</h2>
+        <div className= 'classes.root'>
+
+            <PlantSearchCard
+            data={plantDetails}
+
+
+            />
+            
         </div>
     )
 }
--- src/utils/API.js
+++ src/utils/API.js
@@ -5,7 +5,10 @@ const API = {
         return axios.post("http://localhost:3001/token")
     },
     getDatabasePlants: function (query) {
-        return axios.get("http://localhost:3001/plants/search/"+query)
+        return axios.get("http://localhost:3001/plants/search/" + query)
+    },
+    getPlantID: function (id) {
+        return axios.get("http://localhost:3001/plant/" + id)
     },
     getSearchedPlants: function (query, userToken, page) {
         return axios.get("http://localhost:3001/api/search/"+query+"/"+ userToken + "/" + page)
