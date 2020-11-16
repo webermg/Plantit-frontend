b9937d9558cf8c4bd354d65e35b8b3b8eea3865a
webermg <matthew.gerald.weber@gmail.com>
2020-11-16 13:44:14 -0800

    added userdata load

--- src/Components/Pages/MyGarden/MyGarden.js
+++ src/Components/Pages/MyGarden/MyGarden.js
@@ -2,12 +2,20 @@ import { Container } from '@material-ui/core'
 import React from 'react'
 import Scene from '../../Planner/Scene/Scene'
 import '../MyGarden/MyGarden.css'
+import API from '../../../utils/API'
 
 export default function MyGarden() {
+    const[userData,setUserData]=React.useState()
+
+    React.useEffect(()=>{
+        API.getUser('5fb293ce08c420f0f92a628c').then(res=>setUserData(res.data));
+    },[])
+
+    
     return (
         <Container>
             <h2>my Garden</h2>
-            <Scene/>
+            <Scene userData={userData}/>
         </Container>
     )
 }
