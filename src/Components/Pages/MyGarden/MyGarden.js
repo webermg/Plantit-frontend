473d9c618104c08b3ffb10eb9fc0a8c7ede54ffa
webermg <matthew.gerald.weber@gmail.com>
2020-11-17 10:44:26 -0800

    delay render until userdata is loaded

--- src/Components/Pages/MyGarden/MyGarden.js
+++ src/Components/Pages/MyGarden/MyGarden.js
@@ -1,21 +1,26 @@
 import { Container } from '@material-ui/core'
-import React from 'react'
+import React, { Suspense } from 'react'
 import Scene from '../../Planner/Scene/Scene'
 import '../MyGarden/MyGarden.css'
 import API from '../../../utils/API'
 
 export default function MyGarden() {
-    const[userData,setUserData]=React.useState()
+    const [userData, setUserData] = React.useState(null)
 
-    React.useEffect(()=>{
-        API.getUser('5fb293ce08c420f0f92a628c').then(res=>setUserData(res.data));
-    },[])
+    React.useEffect(() => {
+        API.getUser('5fb293ce08c420f0f92a628c').then(res => {
+            setUserData(res.data);
+            console.log(res.data)
+        });
+    }, [])
 
-    
+    if(!userData) return <h1>loading...</h1>
+
     return (
-        <Container>
-            <h2>my Garden</h2>
-            <Scene userData={userData}/>
-        </Container>
-    )
+    <Container>
+        <h2>my Garden</h2>
+        <Scene userData={userData} />
+    </Container>)
+
+
 }
