8707fd18b0c25005347000e1e0433f6792715b26
Ann Guinee <annmguinee@gmail.com>
2020-11-17 14:19:28 -0800

    Can create comments

--- src/utils/API.js
+++ src/utils/API.js
@@ -7,37 +7,44 @@ const API = {
     getDatabasePlants: function (query) {
         return axios.get("http://localhost:3001/plants/search/" + query)
     },
-    getUser: function(query) {
+    getUser: function (query) {
         return axios.get("http://localhost:3001/user/" + query)
     },
     getPlantID: function (slug) {
         return axios.get("http://localhost:3001/plant/" + slug)
     },
     getSearchedPlants: function (query, userToken, page) {
-        return axios.get("http://localhost:3001/api/search/"+query+"/"+ userToken + "/" + page)
+        return axios.get("http://localhost:3001/api/search/" + query + "/" + userToken + "/" + page)
     },
-    getNewPlant: function(slug, usertoken) {
-        return axios.post("http://localhost:3001/api/slug/"+ slug+ "/" + usertoken)
+    getNewPlant: function (slug, usertoken) {
+        return axios.post("http://localhost:3001/api/slug/" + slug + "/" + usertoken)
     },
 
-    login: function(formData) {
+    login: function (formData) {
         return axios.post("http://localhost:3001/login", formData)
     },
 
-    signup: function(formData) {
+    signup: function (formData) {
         return axios.post("http://localhost:3001/user", formData)
     },
 
-    getFeaturedPlants: function() {
+    getFeaturedPlants: function () {
         return axios.get("http://localhost:3001/findByComments")
     },
 
     favoritePlant: function (plantId, userId) {
         return axios.post("http://localhost:3001/myplants/create", {
-          userId: userId,
-          plantId: plantId,
+            plantId: plantId,
+            userId: userId,
         })
-      }
+    },
+    makeComment: function (plantId, userId,commentText) {
+        return axios.post("http://localhost:3001/comment", {
+            plantId: plantId,
+            userId: userId,
+            commentText: commentText
+        })
+    }
 }
 
 export default API;
