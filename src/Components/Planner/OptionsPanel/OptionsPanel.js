63c1f93fe61cd36ce56c8196ac620b431d6e4bde
webermg <matthew.gerald.weber@gmail.com>
2020-11-19 13:38:04 -0800

    added more options

--- src/Components/Planner/OptionsPanel/OptionsPanel.js
+++ src/Components/Planner/OptionsPanel/OptionsPanel.js
@@ -1,22 +1,61 @@
 import React from 'react';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
+import {Switch,Slider,Typography} from '@material-ui/core';
 
 export default function OptionsPanel(props) {
-  // const [state, setState] = React.useState({
-  //   checkedA: true,
-  //   checkedB: true,
-  // });
-
-  // const handleChange = (event) => {
-  //   setState({ ...state, [event.target.name]: event.target.checked });
-  // };
+  
   return (
     <FormGroup row>
       <FormControlLabel
         control={<Switch checked={props.displayGrid} onChange={props.onChange} name="displayGrid" />}
         label="Display Grid"
+      />
+      <FormControlLabel
+        control={<Switch checked={props.lockBackground} onChange={props.onChange} name="lockBackground" />}
+        label="Lock Background"
+      />
+      <FormControlLabel
+        control={<Switch checked={props.lockForeground} onChange={props.onChange} name="lockForeground" />}
+        label="Lock Foreground"
+      />
+      <FormControlLabel
+        control={<Switch checked={props.gridSnap} onChange={props.onChange} name="gridSnap" />}
+        label="Snap to Grid"
+      />
+      <Typography id="discrete-slider1" gutterBottom>
+        Grid Snap Distance
+      </Typography>
+      <Slider
+        defaultValue={props.snapDist}
+        // getAriaValueText={valuetext}
+        aria-label='snapDist'
+        name="snapDist"
+        aria-labelledby="discrete-slider1"
+        valueLabelDisplay="auto"
+        onChangeCommitted={props.onSliderChange}
+        step={5}
+        marks
+        min={5}
+        max={30}
+        disabled={!props.gridSnap}
+      />
+      <Typography id="discrete-slider2" gutterBottom>
+        Grid Size
+      </Typography>
+      <Slider
+        defaultValue={props.gridSize}
+        // value={props.gridSize}
+        // getAriaValueText={value=>value}
+        aria-label='gridSize'
+        name="gridSize"
+        aria-labelledby="discrete-slider2"
+        valueLabelDisplay="auto"
+        onChangeCommitted={props.onSliderChange}
+        step={10}
+        marks
+        min={20}
+        max={80}
       />
       {/* <FormControlLabel
         control={
