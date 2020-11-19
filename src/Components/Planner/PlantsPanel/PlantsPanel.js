2c9b4ff30ffb85a091ca5ec4fa443be9b898005b
webermg <matthew.gerald.weber@gmail.com>
2020-11-19 09:45:45 -0800

    added key to remove warning

--- src/Components/Planner/PlantsPanel/PlantsPanel.js
+++ src/Components/Planner/PlantsPanel/PlantsPanel.js
@@ -4,7 +4,8 @@ import {Button, ButtonGroup} from '@material-ui/core'
 export default function PlantsPanel(props) {
   return (
     <ButtonGroup orientation='vertical' color="primary" className='object-panel'>
-      {props.myPlants.map(plant => (<Button 
+      {props.myPlants.map((plant,i) => (<Button 
+        key={i}
         onClick={()=>props.onClick({src:'https://cors-anywhere.herokuapp.com/' + plant.image_url,text:plant.common_name})}
         >{plant.common_name}</Button>))}
     </ButtonGroup>
