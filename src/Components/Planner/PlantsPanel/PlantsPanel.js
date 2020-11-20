16f1d42545a498d85adab3f6665b5ba5911e15e6
webermg <matthew.gerald.weber@gmail.com>
2020-11-20 11:04:14 -0800

    handles null images

--- src/Components/Planner/PlantsPanel/PlantsPanel.js
+++ src/Components/Planner/PlantsPanel/PlantsPanel.js
@@ -6,7 +6,11 @@ export default function PlantsPanel(props) {
     <ButtonGroup orientation='vertical' color="primary" className='object-panel'>
       {props.myPlants.map((plant,i) => (<Button 
         key={i}
-        onClick={()=>props.onClick({src:'https://cors-anywhere.herokuapp.com/' + plant.image_url,text:plant.common_name})}
+        onClick={()=>{
+          const imgsrc = plant.image_url !== null ? plant.image_url : 'images/placeholder.png'
+          props.onClick({
+          src:imgsrc,
+          text:plant.common_name})}}
         >{plant.common_name}</Button>))}
     </ButtonGroup>
   )
