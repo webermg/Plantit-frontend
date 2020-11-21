fee57b408b5613e24ff997d68e1837c654a65e01
webermg <matthew.gerald.weber@gmail.com>
2020-11-21 13:10:46 -0800

    added delay on publish to prevent spam

--- src/Components/Planner/Toolbar/Toolbar.js
+++ src/Components/Planner/Toolbar/Toolbar.js
@@ -1,27 +1,43 @@
 import React from 'react'
-import {ButtonGroup, Button, Grid} from '@material-ui/core'
+import { ButtonGroup, Button, Grid, Paper } from '@material-ui/core'
 
 export default function Toolbar(props) {
-  
-  return  (<Grid container xs justify='space-between'>
-  <Grid item xs>
 
-  {props.selectedId && 
-    (<ButtonGroup variant="contained" color="primary" aria-label="outlined primary button group">
-      <Button onClick={() => props.onDelete(props.selectedId)}>Delete</Button>
-      <Button onClick={() => props.toFront(props.selectedId)}>Bring to Front</Button>
-      <Button onClick={() => props.toBack(props.selectedId)}>Send to Back</Button>
-    </ButtonGroup>)}
-  </Grid>
-  <Grid item xs>
-    <ButtonGroup variant="contained" color="primary" aria-label="outlined primary button group">
-      <Button>Undo</Button>
-      <Button>Redo</Button>
-      <Button onClick={props.onPublish}>Publish</Button>
-    </ButtonGroup>
-  </Grid>
+  const [enabled, setEnabled] = React.useState(true)
 
-  </Grid>)
-  
+  const disable = (time) => {
+    setEnabled(false)
+    setTimeout(() => {
+      setEnabled(true)
+    }, time)
+  }
+
+  return (
+
+    // <Paper>
+      <Grid container xs justify='space-between'>
+        <Grid item>
+          {props.selectedId &&
+            (<ButtonGroup variant="contained" color="primary" aria-label="outlined primary button group">
+              <Button onClick={() => props.onDelete(props.selectedId)}>Delete</Button>
+              <Button onClick={() => props.toFront(props.selectedId)}>Bring to Front</Button>
+              <Button onClick={() => props.toBack(props.selectedId)}>Send to Back</Button>
+            </ButtonGroup>)}
+        </Grid>
+        <Grid item>
+          <ButtonGroup variant="contained" color="primary" aria-label="outlined primary button group">
+            <Button>Undo</Button>
+            <Button>Redo</Button>
+            <Button
+              disabled={!enabled}
+              onClick={() => {
+                props.onPublish()
+                disable(5000)
+              }}>Publish</Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    // </Paper>
+  )
   
 }
