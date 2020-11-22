59bd405b9d881447f6e8c46dccd3b2ef29ccb11e
webermg <matthew.gerald.weber@gmail.com>
2020-11-22 14:39:16 -0800

    updated image references

--- src/Components/Planner/DrawPanel/DrawPanel.js
+++ src/Components/Planner/DrawPanel/DrawPanel.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { Button, ButtonGroup } from '@material-ui/core'
 
 export default function DrawPanel(props) {
-  const imgs = ['/images/grass.jpg',
-    '/images/concrete.jpg',
-    '/images/mulch.jpg',
-    '/images/wood.jpg']
+  const imgs = ['/images/background/grass.jpg',
+    '/images/background/concrete.jpg',
+    '/images/background/mulch.jpg',
+    '/images/background/wood.jpg',
+    '/images/background/gravel.jpg',
+    '/images/background/shingle.jpg',
+  ]
   return (
     <ButtonGroup orientation='vertical' className='draw-panel'>
       {imgs.map((img, i)=>(<Button key={i} variant='contained' color={props.active === i+1 ? 'primary' : ''} onClick={() => props.onClick(img,i+1)}>
-        <img src={img} width={100} height={100} alt=""/>
+        <img src={img} width={100} alt=""/>
       </Button>))}
     </ButtonGroup>
   )
